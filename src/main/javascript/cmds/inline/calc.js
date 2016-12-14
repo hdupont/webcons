74debcc3882436538f8450e03380debec5ed680f
hd <dupont.hubert@gmail.com>
2016-12-14 13:57:16 +0100

    Ajout de calc_help

--- src/main/javascript/cmds/inline/calc.js
+++ src/main/javascript/cmds/inline/calc.js
@@ -3,3 +3,15 @@ ns_wconsapp.cmds.inline.calc = function(api) {
 	var res = ns_wconsapp.helpers.calcExpr(expr);
 	api.println(res);
 }
+
+ns_wconsapp.cmds.inline.calc_help = function(api) {
+	api.println("Syntaxe:");
+	api.println("    calc nombre opérateur nombre");
+	api.println("");
+	api.println("où opérateur vaut : +, -, *, /");
+	api.println("où nombre est nombre...");
+	api.println("avec ou sans espace entre les nombres et l'opérateur :)");
+	api.println("");
+	api.println("Par exemple d'utilisation:");
+	api.println("    calc 2 * 3");
+}
