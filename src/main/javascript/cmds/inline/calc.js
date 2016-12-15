7aa3100a5d7454069cc629042a031b48f8257bde
hd <dupont.hubert@gmail.com>
2016-12-15 15:47:03 +0100

    Précision de l'aide de calc

--- src/main/javascript/cmds/inline/calc.js
+++ src/main/javascript/cmds/inline/calc.js
@@ -13,10 +13,11 @@ ns_wconsapp.cmds.help.calc = function(api) {
 	api.println("Description:");
 	api.println("    " + api.cmdName() + " permet d'effectuer une opération entre deux nombres.");
 	api.println("Syntaxe:");
-	api.println("    calc nombre opérateur nombre");
+	api.println("    calc opérande1 opérateur opérande2");
 	api.println("Avec:");
 	api.println("    opérateur: +, -, *, /");
-	api.println("    nombre: un nombre...");
+	api.println("    opérande1: un nombre");
+	api.println("    opérande2: un nombre différent de zéro dans le cas de la division");
 	api.println("Note:");
 	api.println("    Les espaces sont facultatifs entre l'opérateur et ses opérandes.");
 	api.println("Exemples d'utilisation:");
