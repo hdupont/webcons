339497380e52c8a9722bfee243873eb1b8fff816
hdupont <dupont.hubert@gmail.com>
2016-12-17 10:00:37 +0100

    chkbal

--- src/main/javascript/cmds/inline/chkbal.js
+++ src/main/javascript/cmds/inline/chkbal.js
@@ -1,25 +1,54 @@
 ns_wconsapp.cmds.inline.chkbal = function(api) {
-	var parentcnt = 0;
 	var input = api.input();
+	
+	// On détermine de quel caractère on doit vérifier "l'équilibre".
+	var c = input.readToken();
+	var openingChar = "";
+	var closingChar = "";
+	if (c === "(") {
+		openingChar = c;
+		closingChar = ")";	
+	}
+	else if (c === "[") {
+		openingChar = c;
+		closingChar = "]";
+	}
+	else if (c === "{") {
+		openingChar = c;
+		closingChar = "}";
+	}
+	else if (c === "<") {
+		openingChar = c;
+		closingChar = ">";
+	}
+	
+	// On vérifie que l'expression est bien équilibrée.
+	var charCount = 0;
+	var charCountChanged = false;
 	while (! input.isEmpty()) {
 		var c = input.readChar(); 
-		if (c === '(') {
-			parentcnt++;
+		if (c === openingChar) {
+			charCount++;
+			charCountChanged = true;
 		}
-		if (c === ')') {
-			parentcnt--;
+		if (c === closingChar) {
+			charCount--;
+			charCountChanged = true;
 		}
 	}
-	api.println(parentcnt === 0 ? "OK" : "NOT OK");
+	var isBalanced = (charCount === 0) && charCountChanged;
+	api.println(isBalanced ? "OK" : "NOT OK");
 }
 
 ns_wconsapp.cmds.help.chkbal = function(api) {
 	api.println("Description:");
-	api.println("    " + api.cmdName() + " vérifie si une expression est bien parenthésée.");
+	api.println("    " + api.cmdName() + " vérifie si une expression est bien \"équilibrée\".");
 	api.println("Syntaxe:");
-	api.println("    " + api.cmdName() + " exp");
+	api.println("    " + api.cmdName() + " ouvrant exp");
 	api.println("Avec:");
+	api.println("    ouvrant: le caractère ouvrant d'une des paires (), {}, [], <>");
 	api.println("    exp: l'expression à vérifier.");
 	api.println("Exemple d'utilisation:");
-	api.println("    " + api.cmdName() + " ((bonjour))(tout (le))(monde)");
+	api.println("    " + api.cmdName() + " ((hello))(world)");
+	api.println("    " + api.cmdName() + " {{hello}}{world}");
 }
