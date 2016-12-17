ae90959047bcf0383541f71064609dc946d2991e
hdupont <dupont.hubert@gmail.com>
2016-12-17 08:10:33 +0100

    chkpar

---
+++ src/main/javascript/cmds/inline/chkpar.js
@@ -0,0 +1,25 @@
+ns_wconsapp.cmds.inline.chkpar = function(api) {
+	var parentcnt = 0;
+	var input = api.input();
+	while (! input.isEmpty()) {
+		var c = input.readChar(); 
+		if (c === '(') {
+			parentcnt++;
+		}
+		if (c === ')') {
+			parentcnt--;
+		}
+	}
+	api.println(parentcnt === 0 ? "OK" : "NOT OK");
+}
+
+ns_wconsapp.cmds.help.echo = function(api) {
+	api.println("Description:");
+	api.println("    " + api.cmdName() + " vérifie si une expression est bien parenthésée.");
+	api.println("Syntaxe:");
+	api.println("    " + api.cmdName() + " exp");
+	api.println("Avec:");
+	api.println("    exp: l'expression à vérifier.");
+	api.println("Exemple d'utilisation:");
+	api.println("    " + api.cmdName() + " ((bonjour))(tout (le))(monde)");
+}
